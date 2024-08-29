321e57bd06f66403e5be278c8d9124662d6dff0f
francobustos23 <bustosfranco071@gmail.com>
2024-08-29 13:25:01 -0300

    database connection

---
+++ src/DB/Connection.ts
@@ -0,0 +1,16 @@
+import { connect } from "mongoose";
+import { URI } from "../Config/conf";
+
+export const dbConnection = async () => {
+    try {
+        if (URI) {
+            await connect(URI);
+            console.log('🟢 Conexión a la base de datos exitosa 🥸 👌');
+        } else {
+            console.log('NO HAY URI');
+        }
+    } catch (err) {
+        console.log('🔴 Error al conectar la base de datos ', err);
+        process.exit();
+    }
+}
