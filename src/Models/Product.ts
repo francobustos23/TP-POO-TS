d76c563e66f04610d1d7a080fdc5bd9afc6e869f
francobustos23 <bustosfranco071@gmail.com>
2024-08-30 12:09:32 -0300

    Models Products created

---
+++ src/Models/Product.ts
@@ -0,0 +1,19 @@
+import { model, Schema } from "mongoose";
+
+export interface IProduct {
+    name: string,
+    description: string,
+    price: number,
+    category: string
+    stock: number
+}
+
+const productSchema = new Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    category: { type: String, required: true },
+    stock: { type: Number, required: true }
+})
+
+export default model('product', productSchema)
